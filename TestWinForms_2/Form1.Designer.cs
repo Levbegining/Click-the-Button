50531cf6527c5a8887f01d473de506a81d39ea43
Lev <levshapovalov2010@gmail.com>
2025-11-30 00:04:30 +0300

    Initial commit

---
+++ TestWinForms_2/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace TestWinForms_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            CheckBoxCheckMe = new CheckBox();
+            ButtonClick = new Button();
+            Timer = new System.Windows.Forms.Timer(components);
+            LabelTimeGame = new Label();
+            LabelCountClicks = new Label();
+            ButtonRestart = new Button();
+            LabelRestart = new Label();
+            SuspendLayout();
+            // 
+            // CheckBoxCheckMe
+            // 
+            CheckBoxCheckMe.AutoSize = true;
+            CheckBoxCheckMe.BackColor = SystemColors.Control;
+            CheckBoxCheckMe.Location = new Point(444, 269);
+            CheckBoxCheckMe.Name = "CheckBoxCheckMe";
+            CheckBoxCheckMe.Size = new Size(249, 36);
+            CheckBoxCheckMe.TabIndex = 0;
+            CheckBoxCheckMe.Text = "Check Me For Start";
+            CheckBoxCheckMe.TextAlign = ContentAlignment.TopCenter;
+            CheckBoxCheckMe.UseVisualStyleBackColor = false;
+            CheckBoxCheckMe.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // ButtonClick
+            // 
+            ButtonClick.Enabled = false;
+            ButtonClick.Location = new Point(25, 293);
+            ButtonClick.Name = "ButtonClick";
+            ButtonClick.Size = new Size(200, 160);
+            ButtonClick.TabIndex = 1;
+            ButtonClick.Text = "Click";
+            ButtonClick.UseVisualStyleBackColor = true;
+            ButtonClick.Visible = false;
+            ButtonClick.Click += button1_Click;
+            // 
+            // Timer
+            // 
+            Timer.Enabled = true;
+            Timer.Interval = 1000;
+            Timer.Tick += Timer_Tick;
+            // 
+            // LabelTimeGame
+            // 
+            LabelTimeGame.AutoSize = true;
+            LabelTimeGame.Location = new Point(25, 9);
+            LabelTimeGame.Name = "LabelTimeGame";
+            LabelTimeGame.Size = new Size(188, 32);
+            LabelTimeGame.TabIndex = 4;
+            LabelTimeGame.Text = "Time of game: 0";
+            // 
+            // LabelCountClicks
+            // 
+            LabelCountClicks.AutoSize = true;
+            LabelCountClicks.Location = new Point(25, 58);
+            LabelCountClicks.Name = "LabelCountClicks";
+            LabelCountClicks.Size = new Size(167, 32);
+            LabelCountClicks.TabIndex = 5;
+            LabelCountClicks.Text = "Count clicks: 0";
+            // 
+            // ButtonRestart
+            // 
+            ButtonRestart.Location = new Point(469, 217);
+            ButtonRestart.Margin = new Padding(3, 3, 20, 3);
+            ButtonRestart.Name = "ButtonRestart";
+            ButtonRestart.Size = new Size(150, 46);
+            ButtonRestart.TabIndex = 6;
+            ButtonRestart.Text = "Restart";
+            ButtonRestart.UseVisualStyleBackColor = true;
+            ButtonRestart.Click += ButtonRestart_Click;
+            // 
+            // LabelRestart
+            // 
+            LabelRestart.AutoSize = true;
+            LabelRestart.Location = new Point(431, 139);
+            LabelRestart.Name = "LabelRestart";
+            LabelRestart.Size = new Size(262, 32);
+            LabelRestart.TabIndex = 7;
+            LabelRestart.Text = "The time is off! Results:";
+            LabelRestart.TextAlign = ContentAlignment.MiddleCenter;
+            LabelRestart.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(974, 629);
+            Controls.Add(LabelRestart);
+            Controls.Add(ButtonRestart);
+            Controls.Add(LabelCountClicks);
+            Controls.Add(LabelTimeGame);
+            Controls.Add(ButtonClick);
+            Controls.Add(CheckBoxCheckMe);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ClientSizeChanged += Form1_ClientSizeChanged;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private CheckBox CheckBoxCheckMe;
+        private Button ButtonClick;
+        private System.Windows.Forms.Timer Timer;
+        private TextBox TextBoxTime;
+        private TextBox TextBoxCount;
+        private Label LabelTimeGame;
+        private Label LabelCountClicks;
+        private Button ButtonRestart;
+        private Label LabelRestart;
+    }
+}
---
+++ TestWinForms_2/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace TestWinForms_2
+{
+    public partial class Form1 : Form
+    {
+        //private bool isGameStarted = false;
+        private int timeGame = 0;
+        private int countClicks = 0;
+        private int prevCountClicks = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowOnly(CheckBoxCheckMe);
+            Timer.Enabled = true;
+            Timer.Stop();
+
+            CheckBoxCheckMe.ForeColor = Color.Red;
+            CheckBoxCheckMe.Location = new Point(ClientSize.Width / 2 - CheckBoxCheckMe.Width / 2,
+                ClientSize.Height / 2 - CheckBoxCheckMe.Height / 2);
+
+
+            Random rand = new Random();
+            int newX = rand.Next(0, ClientSize.Width - ButtonClick.Width + 1);
+            int newY = rand.Next(0, ClientSize.Height - ButtonClick.Height + 1);
+
+            int red = rand.Next(0, 32) * 8; // Диапазон от 0 до 255
+            int green = rand.Next(0, 32) * 8;
+            int blue = rand.Next(0, 32) * 8;
+
+
+            ButtonClick.Location = new Point(newX, newY);
+            ButtonClick.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CheckBoxCheckMe.Checked)
+            {
+
+            
+
+                CheckBoxCheckMe.ForeColor = Color.Green;
+
+                ShowExcept(CheckBoxCheckMe, LabelRestart, ButtonRestart);
+                Timer.Start();
+                //Timer.Enabled = true;
+
+                //isGameStarted = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Size Button: 200 * 160
+            // Size window: 1000 * 700
+
+            countClicks += 1;
+            LabelCountClicks.Text = $"Count clicks: {countClicks}";
+
+            Random rand = new Random();
+            int newX = rand.Next(0, ClientSize.Width - ButtonClick.Width + 1);
+            int newY = rand.Next(0, ClientSize.Height - ButtonClick.Height + 1);
+
+            int red = rand.Next(0, 256); // Диапазон от 0 до 255
+            int green = rand.Next(0, 256);
+            int blue = rand.Next(0, 256);
+
+
+            ButtonClick.Location = new Point(newX, newY);
+            ButtonClick.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            //CheckBoxCheckMe.Location = new Point(ClientSize.Width / 2 - CheckBoxCheckMe.Width / 2,
+            //    ClientSize.Height / 2 - CheckBoxCheckMe.Height / 2);
+            LocateOnCenter(CheckBoxCheckMe);
+            LocateOnCenter(LabelRestart);
+            ButtonRestart.Location = new Point(ClientSize.Width / 2 - ButtonRestart.Width / 2,
+                ClientSize.Height / 2 + LabelRestart.Height / 2 + 30);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timeGame++;
+            LabelTimeGame.Text = $"Time of game: {timeGame}";
+
+            if (timeGame >= 30)
+            {
+                ShowOnly(ButtonRestart, LabelRestart);
+
+                int maxCountClicks = Math.Max(prevCountClicks, countClicks);
+                LabelRestart.Text = $"The time is off!\nBest result: {maxCountClicks}";
+                LocateOnCenter(LabelRestart);
+                //CheckBoxCheckMe.BackColor = Color.Red;
+                ButtonRestart.Location = new Point(ClientSize.Width / 2 - ButtonRestart.Width / 2,
+                    ClientSize.Height/2 + LabelRestart.Height/2 + 30);
+
+                Timer.Stop();
+                LabelTimeGame.Text = "Time of game: 0";
+                LabelCountClicks.Text = "Count clicks: 0";
+                CheckBoxCheckMe.ForeColor = Color.Red;
+                CheckBoxCheckMe.Checked = false;
+
+                prevCountClicks = maxCountClicks;
+
+                timeGame = 0;
+                countClicks = 0;
+            }
+        }
+
+        private void ButtonRestart_Click(object sender, EventArgs e)
+        {
+            ShowOnly(CheckBoxCheckMe);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // NOTHING TO DO
+        }
+
+        public void ShowOnly(params Control[] allowed)
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+                if (!allowed.Contains(ctrl))
+                {
+                    ctrl.Enabled = false;
+                    ctrl.Visible = false;
+                }
+                else
+                {
+                    ctrl.Enabled = true;
+                    ctrl.Visible = true;
+                }
+            }
+        }
+
+        public void ShowExcept(params Control[] notAllowed)
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+                if (notAllowed.Contains(ctrl))
+                {
+                    ctrl.Enabled = false;
+                    ctrl.Visible = false;
+                }
+                else
+                {
+                    ctrl.Enabled = true;
+                    ctrl.Visible = true;
+                }
+            }
+        }
+
+        public void LocateOnCenter(Control ctrl)
+        {
+            ctrl.Location = new Point(ClientSize.Width / 2 - ctrl.Width / 2,
+                ClientSize.Height / 2 - ctrl.Height / 2);
+        }
+    }
+}
